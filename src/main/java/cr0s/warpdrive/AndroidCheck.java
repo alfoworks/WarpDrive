00f7d133e746d299840ee909ba192e92aca14908
Rob <rob.bog@list.ru>
2020-04-30 01:34:19 +0300

    [FIX] Ok, lemme fix that

--- src/main/java/cr0s/warpdrive/AndroidCheck.java
+++ src/main/java/cr0s/warpdrive/AndroidCheck.java
@@ -3,7 +3,7 @@ package cr0s.warpdrive;
 import matteroverdrive.entity.player.MOPlayerCapabilityProvider;
 import net.minecraft.entity.player.EntityPlayer;
 
-public class AndroidCheck {
+public class AndroidCheckHelper {
 	public static boolean isAndroid(EntityPlayer player) {
 		return MOPlayerCapabilityProvider.GetAndroidCapability(player) != null && MOPlayerCapabilityProvider.GetAndroidCapability(player).isAndroid();
 	}
